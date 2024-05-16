442b33d8db1c12afabc627002fbe48c1723fa0c3
Janith Wathsala <janijaniwathsala@gmail.com>
2024-05-16 23:30:51 +0530

    write the code for interface of a application

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ root = tk.Tk()
 root.geometry("400x200")
 root.title("Encode password")
 
+ent_pwd = ttk.Label(root, text="Enter your password", font=("arial", 10, "bold"))
+ent_pwd.pack()
+
+frame = ttk.Frame(root, width=400, height=60)
+frame.pack()
+
+task_entry = ttk.Entry(frame, font="arial 18", width=27)
+task_entry.pack()
+
+
+encode_button = ttk.Button(root, text="Encode")
+encode_button.pack(pady=5)
+
+encode_num = ttk.Label(root, text="", font=("arial", 10))
+encode_num.pack()
+
 root.mainloop()
  
 
