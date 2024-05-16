a7e7e50aec4c3db4e1d3a54ec33b9750117a5c7a
Janith Wathsala <janijaniwathsala@gmail.com>
2024-05-16 23:49:11 +0530

    Writing a code to encode the entered password

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 import base64
 
+def add_task(event=None):
+    password = task_entry.get()
+    encode_password = base64.b64encode(password.encode()).decode()
+    encode_num.config(text=f"Encode password:{encode_password}")
+
 root = tk.Tk()
 root.geometry("400x200")
 root.title("Encode password")
@@ -15,8 +20,9 @@ frame.pack()
 task_entry = ttk.Entry(frame, font="arial 18", width=27)
 task_entry.pack()
 
+task_entry.bind("<Return>", add_task)
 
-encode_button = ttk.Button(root, text="Encode")
+encode_button = ttk.Button(root, text="Encode", command=add_task)
 encode_button.pack(pady=5)
 
 encode_num = ttk.Label(root, text="", font=("arial", 10))
